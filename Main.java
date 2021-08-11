1f1f6b508c64c923665f149e95b0ce8f8b5a5837
Oleg Atlasov <olegkui@mail.ru>
2021-08-11 10:51:28 +0400

    Add Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 3000;
+        int priceMile = 20;
+        int countMiles = priceTicket / priceMile;
+        System.out.println(countMiles);
+    }
+}
